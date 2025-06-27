095abbc016224b27412c8294f4e976c80a4ebbac
Winston Li <winstoon@berkeley.edu>
2025-06-26 18:32:04 -0700

    changing db configs, began making apis

--- app/routes/articles.py
+++ app/routes/articles.py
@@ -3,3 +3,19 @@ from app.models import Article
 from app.database import SessionLocal
 
 router = APIRouter()
+
+@router.get("/articles")
+def get_articles(keyword: ):
+    db = SessionLocal()
+    try:
+        if keyword:
+            return db.query(Article).filter_by(keyword=keyword).all()
+        return db.query(Article).all()
+    finally:
+        db.close()
+
+@router.get("/status")
+def status():
+    return {"status": "ok"}
+
+
